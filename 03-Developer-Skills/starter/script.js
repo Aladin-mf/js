74159d80256f7390a10a70f7e37752ae7031d079
Aladin-mf <aladinmf01@gmail.com>
2021-04-21 15:58:45 +0600

    ................

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,14 +1,27 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const mKelvin = function(){
-const m ={
-    type:'temp',
-    unit:`celsius`,
-    value:33
+// const mKelvin = function(){
+// const m ={
+//     type:'temp',
+//     unit:`celsius`,
+//     value:33
 
+// }
+// const kelvin= m.value+273
+// return kelvin
+// }
+// console.log(mKelvin());
+
+
+
+const arr = [17,21,23]
+const printForecast = function(arr){
+    let str = ` `
+    for(let i =0;i<arr.length;i++){
+        str+=`${arr[i]}*C in ${1+i} days... `
+    }
+    console.log(`.....`+str);
+    
 }
-const kelvin= m.value+273
-return kelvin
-}
-console.log(mKelvin());
+printForecast(arr)
