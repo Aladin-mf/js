d062e02bf03d211abab56292b8aea9b1fdf1e8b6
Aladin-mf <aladinmf01@gmail.com>
2021-04-26 09:39:30 +0600

    new

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,9 +1,9 @@
 'use strict';
 //selecting elements
 const score0El = document.querySelector(`#score--0`);
+const score1El = document.querySelector(`#score--1`);
 const player0El = document.querySelector(`.player--0`);
 const player1El = document.querySelector(`.player--1`);
-const score1El = document.querySelector(`#score--1`);
 const current0El = document.querySelector(`#current--0`);
 const current1El = document.querySelector(`#current--1`);
 const diceEl = document.querySelector(`.dice`);
@@ -83,10 +83,11 @@ btnHold.addEventListener(`click`, function(){
 btnNew.addEventListener(`click`,function(){
    diceEl.classList.add(`hidden`);
 
-document.querySelector(`#current--${activePlayer}`).textContent = 0;
+document.querySelector(`#current--1`).textContent = 0;
 currentScore = 0;
 play=true
-document.querySelector(`#score--${activePlayer}`).textContent = 0;
+document.querySelector(`#score--0`).textContent = 0;
+document.querySelector(`#score--1`).textContent = 0;
 player0El.classList.remove(`player--winner`)
 player1El.classList.remove(`player--winner`);
 player0El.classList.add(`player--active`);
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,68 @@
 'use strict';
+// /* const name = `aladin`
+
+// function first(){
+
+//     const age =20
+//     if(age>=20){
+//         const dec =2
+//         var mill=true
+//     }
+//     function sec(){
+//          const job = `teacher`
+//          console.log(`${name} is a ${age} old ${job}`)
+//          ;
+
+//     }
+//     sec()
+    
+// }
+// first()
+//  */
+// /* 
+// const aladin = {
+//   year: 2002,
+//   calcage: function () {
+//     console.log(2021 - this.year);
+//   },
+//   fName:`laden`,
+//   greet :function(){
+//       console.log(`hey ${this.fName}`);
+      
+//   }
+// };
+
+// const paden = {
+//   year: 2000,
+  
+// };
+
+
+// aladin.greet() */
+
+let lastName = `sheikh`;
+let oldLastName = lastName;
+lastName = `solomon`;
+console.log(lastName,oldLastName);
+
+
+
+const fima = {
+    fName : `fima`,
+    lName:`sheikh`,
+    age : 18
+}
+const marriedFima = fima
+marriedFima.lName= `rashed`
+console.log(`beffr maarrige:`, fima);
+console.log(`aftr maarrige:`, marriedFima);
+
+const fima2 = {
+    fName : `fima`,
+    lName:`sheikh`,
+    age : 18
+}
+const fimaCopy = Object.assign({},fima2)
+fimaCopy.lName=`rashed`
+console.log(`beffr maarrige:`, fima2);
+console.log(`aftr maarrige:`, fimaCopy);
