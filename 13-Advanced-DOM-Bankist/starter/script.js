b893d780f0715ce6f9ca934f641d967207d426b3
Aladin-mf <aladinmf01@gmail.com>
2021-07-08 22:38:15 +0600

    coment

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -316,6 +316,7 @@ dotContainer.addEventListener('click', function (e) {
 slider()
 //rgb(255,255,255)
 
+/* added new line  */
 // const randomInt = (min , max)=>Math.floor(Math.random()*(max-min+1)+min)
 // const randomColor = () =>
 //   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
